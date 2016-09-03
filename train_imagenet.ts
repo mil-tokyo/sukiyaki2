7ab308faef72492b80f418c6b5b90da78db79bb5
Hidaka Masatoshi <hidaka@mi.t.u-tokyo.ac.jp>
2016-09-03 13:48:24 +0900

    insert data augmentation

--- train_imagenet.ts
+++ train_imagenet.ts
@@ -11,8 +11,11 @@ function train_imagenet(load_weight: boolean = false, cl: boolean = false) {
   }
   // AlexNet + BN
   var layers = [
-    { name: "d_train", type: "blob_data", params: { "file_prefix": "imagenet/train_100k", "data_shape": [227, 227, 3] }, inputs: ["batch"], outputs: ["data", "label"], phase: ["train"] },
-    { name: "d_test", type: "blob_data", params: { "file_prefix": "imagenet/val", "data_shape": [227, 227, 3] }, inputs: ["batch"], outputs: ["data", "label"], phase: ["test"] },
+    { name: "d_train", type: "blob_data", params: { "file_prefix": "imagenetraw/train_shuffle", "data_shape": [256, 256, 3] }, inputs: ["batch"], outputs: ["rawdata", "label"], phase: ["train"] },
+    { name: "d_test", type: "blob_data", params: { "file_prefix": "imagenetraw/val", "data_shape": [256, 256, 3] }, inputs: ["batch"], outputs: ["rawdata", "label"], phase: ["test"] },
+
+    { name: "aug_train", type: "data_augmentation", params: { "out_shape": [227, 227], "scale": 0.00390625, "random_crop": true, "random_flip": true, "input_klass": "uint8" }, inputs: ["rawdata"], outputs: ["data"], phase: ["train"] },
+    { name: "aug_test", type: "data_augmentation", params: { "out_shape": [227, 227], "scale": 0.00390625, "random_crop": false, "random_flip": false, "input_klass": "uint8" }, inputs: ["rawdata"], outputs: ["data"], phase: ["test"] },
 
     { name: "conv1", type: "convolution_2d", params: { in_size: 3, out_size: 96, ksize: [11, 11], stride: [4, 4], pad: [0, 0], bias: false }, inputs: ["data"], outputs: ["conv1"] },
     { name: "bn1", type: "batch_normalization", params: { in_size: 96, target_dim: 3 }, inputs: ["conv1"], outputs: ["bn1"] },
@@ -53,20 +56,24 @@ function train_imagenet(load_weight: boolean = false, cl: boolean = false) {
       net.to_cl();
     }
     net.layer_time = {};
-    var opt = new OptimizerSGD(net, 1e-3);
+
+    var data_mean = $M.permute($M.npyread(fs.readFileSync('./imagenet_mean.npy')), [2, 3, 1]);//to h, w, c
+    (<Sukiyaki.Layers.DataAugmentationLayer>net.layer_instances["aug_train"]).set_data_mean(data_mean);
+    (<Sukiyaki.Layers.DataAugmentationLayer>net.layer_instances["aug_test"]).set_data_mean(data_mean);
+    var opt = new Sukiyaki.Optimizers.OptimizerMomentumSGD(net, 1e-3, 0.9);
     var batch_size = 128;
 
     if (load_weight) {
       console.log('loading net');
-      var buf = new Uint8Array(fs.readFileSync('/tmp/sukiyaki_weight.bin').buffer);
+      var buf = new Uint8Array(fs.readFileSync('/tmp/sukiyaki_weight_alexbn.bin').buffer);
       ArraySerializer.load(buf, net);
     }
 
     var iter = 0;
-    var max_iter = 10000;
+    var max_iter = 100000;
     var next_iter = () => {
       if (iter % 10 == 0) {
-        console.log("iteration " + iter);
+        console.log("iteration " + iter + ' ' + (new Date()));
         if (cl) {
           console.log("buffers: " + $M.CL.buffers);
         }
@@ -84,11 +91,12 @@ function train_imagenet(load_weight: boolean = false, cl: boolean = false) {
               console.log(key + '\t' + element);
             }
           }
+          console.log('cl readcount: ' + $M.CL.readcount);
         }
         opt.release();
         if (iter < max_iter) {
           iter++;
-          if (iter % 100 == 0) {
+          if (iter % 1000 == 0) {
             validation();
           } else {
             next_iter();
@@ -111,7 +119,7 @@ function train_imagenet(load_weight: boolean = false, cl: boolean = false) {
         net.release();
         console.log('saving net');
         var buf = ArraySerializer.dump(net);
-        fs.writeFileSync('/tmp/sukiyaki_weight.bin', new Buffer(buf));
+        fs.writeFileSync('/tmp/sukiyaki_weight_alexbn_' + iter + '.bin', new Buffer(buf));
         if (iter < max_iter) {
           next_iter();
         }
